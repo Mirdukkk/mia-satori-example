25695c4ab85db7d8d0e1a8b3499ec5ebc2a80ecd
mirdukkk <love@deleter.xyz>
2024-04-21 22:04:55 +0300

    максоним

--- index.ts
+++ index.ts
@@ -4,10 +4,13 @@ import { Image } from './image'
 import { Transformer } from '@napi-rs/image'
 import fs from 'fs'
 import { promisify } from 'util'
+import dotenv from 'dotenv'
+import * as os from 'os'
+dotenv.config()
 
 const readFileAsync = promisify(fs.readFile)
 
-const TOKEN: string = 'AAAAA'
+const TOKEN = process.env.TOKEN as string
 
 const app = DiscordFactory.create(TOKEN, {
     gateway: {
@@ -19,7 +22,7 @@ app.on('messageCreate', async context => {
     const { message } = context
 
     if (message.content === 'mia!ping') {
-        return message.reply(`Pong! ${app.gateway.ping}ms`)
+        return message.reply(`Pong@${os.hostname()}! ${app.gateway.ping}ms`)
     }
 
     if (message.content === 'mia!profile') {
@@ -53,7 +56,7 @@ app.on('messageCreate', async context => {
         const image = await transformer.png()
 
         const after = Date.now()
-        return message.reply(`Твой профиль! (render: ${after-before}ms) (react: ${afterReact-before}ms)`,{
+        return message.reply(`Твой профиль@${os.hostname()}! (render: ${after-before}ms) (react: ${afterReact-before}ms)`,{
             files: [
                 {
                 name: 'profile.png',
